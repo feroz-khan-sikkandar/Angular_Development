7e645ebba3978f909de643873f13ecb2fbda223f
feroz-khan-sikkandar <feroz.khan.sikkandar@gmail.com>
2018-11-28 15:20:42 +0530

    c

--- customerApplication/src/CustomerApp/CustomerApp.component.ts
+++ customerApplication/src/CustomerApp/CustomerApp.component.ts
@@ -10,4 +10,13 @@ export class CustomerComponent {
   title = 'app';
 
   CustomerModel:Customer = new Customer();
+
+  CustomerModels:Array<Customer> = new Array<Customer>();
+
+  addCustomer(){
+
+    this.CustomerModels.push(this.CustomerModel);
+    this.CustomerModel = new Customer();
+    
+  }
 }
